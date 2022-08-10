ddc40cd63361a83b7fc73435ce1205ac93d34dff
Fabio Salinas <fabio.salinas1982@gmail.com>
2022-08-10 10:04:50 -0500

    Adding 2022-08-10 advances

--- src/leer_archivos.py
+++ src/leer_archivos.py
@@ -1,5 +1,31 @@
+# %%
+
+from typing import Dict, List, Tuple, Any
 import pandas as pd
 
-def leer_df(ruta_archivo, sep, encabezados):
-    df = pd.read_csv(ruta_archivo, sep = sep, header=None, names=encabezados)
+# %%
+
+def leer_df(ruta_archivo: str, sep: str, encabezados: List[str] = None) -> pd.DataFrame:
+    """
+    Esta es una función que lee archivos y devuelve un dataframe de pandas
+
+    Args:
+        ruta_archivo (str): Ruta completa al archivo que se desea leer
+        sep (str): Caracter separador de campos por fila
+        encabezados (List[str]): Lista de nombres de los encabezados del archivo,
+                                 si no se envía un valor, se tomarán los encabezados
+                                 del archivo
+
+    Returns:
+        df (pandas.DataFrame): Dataframe que contiene toda la data del archivo leido
+    """
+    if encabezados:
+        df = pd.read_csv(ruta_archivo, sep = sep, header=None, names=encabezados)
+    else:
+        df = pd.read_csv(ruta_archivo, sep = sep)
+
+    filas, columnas = df.shape
+
+    print("El dataframe tiene {x} fila(s) y {y} columna(s)".format(x=filas, y=columnas))
+
     return df
--- src/perfilador.py
+++ src/perfilador.py
@@ -1,13 +1,17 @@
 # In[1]
 
+from typing import Dict, List, Tuple, Any
 from pandas_profiling import ProfileReport
 from datetime import datetime
 
 from leer_archivos import leer_df
 
 # In[2]
+reports = "/home/ubuntu/workspace/fsalinas/generador_perfiles_data/reports"
 ruta = "/home/ubuntu/workspace/fsalinas/generador_perfiles_data/DATOS.txt"
 
-df = leer_df(ruta, "|", ["id", "nombre", "apellido", "fecha_nacimiento", "direccion", "telefono", "email"])
+df = leer_df(ruta, "|")
 df.info()
-# reporte = ProfileReport()
+reporte = ProfileReport(df, title="Reporte de datos", minimo = True)
+
+reporte.to_file("/home/ubuntu/workspace/fsalinas/generador_perfiles_data/reporte_datos.html")
